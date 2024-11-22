24ce3fb21bd264a9a32e142e72abe404d0250aff
Phil Tracy <ptracy@northwestern.edu>
2024-11-22 16:27:16 -0600

    lb 10

--- main.tf
+++ main.tf
@@ -61,10 +61,8 @@ module "alb" {
       backend_port        = 80
       target_type = "instance"
       targets = [
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
+        target_id = aws_instance.blog.id
+        port      = 80
       ]
     }
   ]
